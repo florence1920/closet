3abddcbafec63208a8e9f7f708a9b3782610edc0
lsh <jetaime12121@gmail.com>
2025-05-05 18:52:55 +0900

    Feat: localstroage 경로 수정

--- js/initData.js
+++ js/initData.js
@@ -1,12 +1,26 @@
 export function initializeClosetData() {
-  if (localStorage.getItem("closet")) return Promise.resolve();
+  // 이미 데이터가 있으면 바로 종료
+  if (
+    localStorage.getItem("closet") &&
+    JSON.parse(localStorage.getItem("closet")).length > 0
+  ) {
+    return Promise.resolve();
+  }
 
-  return fetch("../data/data.json")
-    .then((res) => res.json())
+  // 데이터 파일 로드
+  return fetch("./data/data.json")
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("데이터 파일을 찾을 수 없습니다.");
+      }
+      return res.json();
+    })
     .then((data) => {
       localStorage.setItem("closet", JSON.stringify(data));
     })
     .catch((err) => {
       console.error("데이터 초기화 중 오류 발생:", err);
+      // 오류 발생 시 빈 배열 초기화
+      localStorage.setItem("closet", JSON.stringify([]));
     });
 }
