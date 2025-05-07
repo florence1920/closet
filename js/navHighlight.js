1711f1ef8bb9cede13a654cdcb2163d42eec3cf0
lsh <jetaime12121@gmail.com>
2025-05-07 17:06:29 +0900

    FIx: 네비게이션 중첩 문제 해결

--- js/navHighlight.js
+++ js/navHighlight.js
@@ -7,8 +7,8 @@ export function navHighlight() {
   const repoNameMatch = baseHref ? baseHref.match(/\/([^\/]+)\/$/) : null;
   const repoName = repoNameMatch ? `/${repoNameMatch[1]}` : "";
 
-  // 로컬 스토리지에서 현재 활성화된 메뉴 확인
-  const activeMenu = localStorage.getItem("activeMenu");
+  // 모든 링크에서 active 클래스 제거
+  navLinks.forEach((link) => link.classList.remove("active"));
 
   navLinks.forEach((link) => {
     link.removeEventListener("click", handleNavClick);
@@ -25,11 +25,9 @@ export function navHighlight() {
         currentPath === `${repoName}/`) &&
         (linkHref === "./index.html" || linkHref === "index.html"));
 
-    // 현재 페이지와 일치하거나 로컬 스토리지에 저장된 active 메뉴와 일치하면 active 클래스 추가
-    if (isMatch || linkHref === activeMenu) {
+    // 현재 페이지와 일치하는 경우에만 active 클래스 추가
+    if (isMatch) {
       link.classList.add("active");
-    } else {
-      link.classList.remove("active");
     }
 
     link.addEventListener("click", handleNavClick);
@@ -44,7 +42,4 @@ function handleNavClick(e) {
 
   // 클릭한 링크에 active 클래스 추가
   this.classList.add("active");
-
-  // 클릭한 링크 href를 로컬 스토리지에 저장
-  localStorage.setItem("activeMenu", this.getAttribute("href"));
 }
